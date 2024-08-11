5132baf07c3e8c14116c4d944ba21c1592db217c
xardasmat <3667700+xardasmat@users.noreply.github.com>
2024-08-11 09:05:23 +0200

    Add best subsequence problem

---
+++ best_subsequece.py
@@ -0,0 +1,16 @@
+import unittest
+
+def find_max_sum(sequence):
+    #TODO(implement this function pretty please \(^_^)/ )
+    # find the maximum of contionus subsequence of provided sequence
+    return None # this does not work, do it correctly please
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_some_sequence(self):
+        self.assertEqual(find_max_sum([1, 0, -4, 2, 0, 1, 2, -3, 2, 2]), 1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
